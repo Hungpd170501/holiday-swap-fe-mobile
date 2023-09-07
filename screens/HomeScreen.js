845c0749d66659bf90b5e893298b8954d9513c22
Tin Nguyen <tinitgithub@gmail.com>
2023-09-07 17:23:28 +0700

    Search Component

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -5,6 +5,7 @@ import { ScrollView, View, Text, SafeAreaView } from "react-native";
 import * as Icon from "react-native-feather";
 import Input from "../components/Input";
 import { TextInput } from "react-native";
+import SearchBar from "../components/SearchBar";
 
 const HomeScreen = () => {
   return (
@@ -40,18 +41,7 @@ const HomeScreen = () => {
         </View>
 
         {/* Search */}
-        <View className="flex-row items-center space-x-2 px-4 pb-2 mt-4">
-          <View className="flex-row flex-1 items-center p-3 rounded-full border border-gray-300 bg-slate-100">
-            <Icon.Search height={25} width={25} stroke="gray" />
-            <TextInput
-              placeholder="Search"
-              className="ml-2 flex-1 text-slate-600"
-            />
-            <View className="flex-row items-center space-x-1 bottom-0 border-l-2 pl-2 border-l-gray-300">
-              <Icon.Sliders height={20} width={20} stroke="gray" />
-            </View>
-          </View>
-        </View>
+        <SearchBar />
 
         {/* Frequently visited */}
       </ScrollView>
