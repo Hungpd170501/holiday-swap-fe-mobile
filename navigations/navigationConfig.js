9f963ad0c9eb58b40ab4a40ef9484434dacc848f
Bùi Trí Thức <buitrithuc1008@gmail.com>
2023-09-07 09:50:25 +0700

    HotelDetailScreen_UI_Thuc

--- navigations/navigationConfig.js
+++ navigations/navigationConfig.js
@@ -6,6 +6,8 @@ import WelcomeScreen from "../screens/WelcomeScreen";
 import WelcomeBackScreen from "../screens/WelcomeBackScreen";
 import SignInScreen from "../screens/SignInScreen";
 import CreateAccountScreen from "../screens/CreateAccountScreen";
+import ProfileScreen from "../screens/ProfileScreen";
+import HotelDetailScreen from "../screens/HotelDetailScreen";
 
 const Stack = createStackNavigator();
 
@@ -13,13 +15,16 @@ function Navigation() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="Loading"
+        // initialRouteName="Loading"
+        initialRouteName="HotelDetailScreen"
         screenOptions={{ headerShown: false }}
       >
         <Stack.Screen name="Loading" component={LoadingScreen} />
         <Stack.Screen name="WelcomeScreen" component={WelcomeScreen} />
         <Stack.Screen name="WelcomeBackScreen" component={WelcomeBackScreen} />
         <Stack.Screen name="SignInScreen" component={SignInScreen} />
+        <Stack.Screen name="ProfileScreen" component={ProfileScreen} />
+        <Stack.Screen name="HotelDetailScreen" component={HotelDetailScreen} />
         <Stack.Screen
           name="CreateAccountScreen"
           component={CreateAccountScreen}
