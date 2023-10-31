1e61b9c059494df48f85cf450dd84e76914dd382
Bùi Trí Thức <buitrithuc1008@gmail.com>
2023-10-31 18:23:15 +0700

    Refactor Profile

--- redux/reducers/userReducers.js
+++ redux/reducers/userReducers.js
@@ -16,7 +16,7 @@ export const userReducers = (state = { user: {}, userProfile: {} }, action) => {
 
     // Gửi login request, chưa đăng nhập nên authen = false
 
-    case LOGIN_SUCCESS: // login thành công authen = true. action.payload là data của th Usẻr
+    case LOGIN_SUCCESS:
       return {
         ...state,
         loading: false,
@@ -30,7 +30,7 @@ export const userReducers = (state = { user: {}, userProfile: {} }, action) => {
         isAuthenticated: true,
         userProfile: action.payload,
       };
-    case LOGIN_FAIL: //login fail authen = false vì đăng nhập k thành công. trả về eror action.payload.message (của error BE set)
+    case LOGIN_FAIL:
       return {
         loading: false,
         isAuthenticated: flase,
